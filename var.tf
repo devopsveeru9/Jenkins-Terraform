f03b69d2bbf0b762a5fdf0281605db8f6d744e14
devopsveeru9 <gangamma.veera@gmail.com>
2022-08-18 18:17:33 +0530

    variable updated

---
+++ var.tf
@@ -0,0 +1,21 @@
+variable "subnet" {
+  type = string
+  default = "subnet-015c2ebd969893cc3"
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type = string
+  default = "ami-090fa75af13c156b4"
+}
+
+
+variable "int_type" {
+  type = string
+  default = "t2.micro"
+}
+
